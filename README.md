3a2be3da2069c5a219c481430cefa59292732fd6
Mateusz Szostok <szostok.mateusz@gmail.com>
2023-05-06 22:32:26 +0200

    Setup docusaurus

---
+++ README.md
@@ -0,0 +1,15 @@
+# Docu Verse
+ 
+This is the central hub that is responsible for publishing documentation for all projects that I own.
+
+## About
+
+Refreshed by GitHub Action, built using [Docusaurus](https://docusaurus.io/) and hosted using [Cloudflare Pages](https://pages.cloudflare.com/).
+
+### Running Locally
+
+To run the documentation locally, follow these steps:
+
+1. Clone this repository: `git clone https://github.com/mszostok/docu-verse.git`
+2. Install dependencies: `cd docu-verse && npm install`
+3. Start the development server: `npm start`
---
+++ docusaurus.config.js
@@ -0,0 +1,128 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: 'Documentation',
+    tagline: 'Documentation',
+    favicon: 'img/favicon.ico',
+
+    // Set the production url of your site here
+    url: 'https://szostok.io',
+    baseUrl: '/docs',
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
+
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // // Please change this to your repo.
+                    // // Remove this to remove the "edit this page" links.
+                    // editUrl:
+                    //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
+    ],
+    plugins: [
+        [
+            '@docusaurus/plugin-content-docs',
+            {
+                id: 'version',
+                path: 'version',
+                routeBasePath: 'version',
+                sidebarPath: require.resolve('./sidebars.version.js'),
+                // ... other options
+            },
+        ],
+    ],
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // Replace with your project's social card
+            image: 'img/docusaurus-social-card.jpg',
+            navbar: {
+                title: 'DOCS',
+                items: [
+                    {
+                        type: 'docSidebar',
+                        docsPluginId: "version",
+                        sidebarId: 'versionSidebar',
+                        position: 'left',
+                        label: 'Version',
+                    },
+                    {
+                        href: 'https://github.com/facebook/docusaurus',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Version',
+                        items: [
+                            {
+                                label: 'Tutorial',
+                                to: '/version/intro',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'Stack Overflow',
+                                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                            },
+                            {
+                                label: 'Discord',
+                                href: 'https://discordapp.com/invite/docusaurus',
+                            },
+                            {
+                                label: 'Twitter',
+                                href: 'https://twitter.com/docusaurus',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'More',
+                        items: [
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/facebook/docusaurus',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Mateusz Szostok`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
+};
+
+module.exports = config;
